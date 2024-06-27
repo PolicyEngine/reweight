c20ea641f3b272e32d6c5bd789195aa1929bd096
= <=>
2024-06-27 09:45:23 -0400

    Updated .gitignore and reformatted some files

--- .github/get-changelog-diff-old.sh
+++ .github/get-changelog-diff-old.sh
@@ -1,3 +1,5 @@
+#Old script, based on existing PolicyEngine code base. Does not work, at least for initial PRs
+
 git remote add upstream https://github.com/policyengine/reweight
 git fetch --tags upstream
 last_tagged_commit=`git describe --tags --abbrev=0 --first-parent`
