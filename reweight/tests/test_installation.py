ad6a2c0c1e5c7607fe8a1f1175b6d8f048b9937e
= <=>
2024-07-16 05:37:48 -0400

    Added a secret usage test

--- reweight/tests/test_installation.py
+++ reweight/tests/test_installation.py
@@ -15,9 +15,7 @@ def test_install():
         raise AssertionError("Failed to build reweight")
 
 
-"""
 def test_secret_usage():
     import os
     print("Token exists:", "POLICYENGINE_GITHUB_MICRODATA_AUTH_TOKEN" in os.environ)
     print("Token length:", len(os.environ.get("POLICYENGINE_GITHUB_MICRODATA_AUTH_TOKEN", "")))
-"""
