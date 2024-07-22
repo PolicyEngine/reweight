132a0041534c7ea7f31cd7c8b56849ddaa0cfb22
= <=>
2024-07-22 09:55:00 -0400

    Added a new test for the reweight logic

--- reweight/tests/test_uk_prototype.py
+++ reweight/tests/test_uk_prototype.py
@@ -3,3 +3,25 @@ def test_uk_microsimulation():
 
     # Create a Microsimulation instance
     sim = Microsimulation()
+
+def test_uk_prototype():
+    from policyengine_uk import Microsimulation
+    from reweight.logic import reweight
+    import torch
+    sim = Microsimulation()
+    
+    from policyengine_uk.data import RawFRS_2021_22
+    RawFRS_2021_22().download()
+
+    from policyengine_uk.data.datasets.frs.calibration.calibrate import generate_model_variables
+    (
+        household_weights,
+        weight_adjustment,
+        values_df,
+        targets,
+        targets_array,
+        equivalisation_factors_array
+    ) = generate_model_variables("frs_2021", 2025)
+
+    sim_matrix = torch.tensor(values_df.to_numpy(), dtype=torch.float32)
+    reweight.reweight(household_weights, sim_matrix, targets, targets_array)
